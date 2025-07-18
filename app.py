6476fb918c8663e82e3a24502035b5370ecdead3
Asish-san <mysol4570@gmail.com>
2025-07-18 13:29:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -310,7 +310,7 @@ st.markdown("""
     <br>
     <span style='color:#43c6ac;'>Streamlit Web App</span> &nbsp; <span style='font-size:24px;'>🌐</span>
     <br>
-    <span style='font-size:14px;'><b>© 2025 Asish Kumar. All rights reserved.<br>This app and its contents are protected under copyright and anti-plagiarism systems. Unauthorized copying, distribution, or use may be detected and flagged. For more info, see <a href='https://docs.github.com/en/site-policy/content-removal-policies/github-copyright-policy' target='_blank'>GitHub Copyright Policy</a>.</b></span>
+    <span style='font-size:14px;'><b>© 2025 Asish Kumar. All rights reserved.</b></span>
     </span>
 </div>
 """, unsafe_allow_html=True)
